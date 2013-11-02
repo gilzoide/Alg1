fa092c9d9229ad695f84a12cedbf2fc6872ec978
LeoSampaio <leosfr1419@Leo-Sampaio-MacBook-Air.local>
2013-11-02 16:53:20 -0200

    Just Testing

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  T1
+//
+//  Created by Léo Sampaio on 02/11/13.
+//  Copyright (c) 2013 Léo Sampaio. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
